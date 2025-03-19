e8911d63241bbb52b1d8f6539c1540fb73104a65
Alexandru Calin <alexandrucalin@proton.me>
2025-03-19 16:20:54 +0200

    Created register form page

--- main.go
+++ main.go
@@ -12,8 +12,11 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.ParseFS(ui.FS, "base.html"))
+	tpl := views.Must(views.ParseFS(ui.FS, "base.html", "home.html"))
 	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.ParseFS(ui.FS, "base.html", "register.html"))
+	r.Get("/register", controllers.StaticHandler(tpl))
 
 	http.ListenAndServe(":4000", r)
 }
