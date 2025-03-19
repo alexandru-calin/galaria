3d9fdf446d5813596a7ee185a20bd420784f1750
Alexandru Calin <alexandrucalin@proton.me>
2025-03-19 15:43:05 +0200

    Added chi package as router

--- main.go
+++ main.go
@@ -5,19 +5,20 @@ import (
 
 	"github.com/alexandru-calin/galaria/ui"
 	"github.com/alexandru-calin/galaria/views"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
 
 	tpl, err := views.ParseFS(ui.FS, "base.html")
 	if err != nil {
 		panic(err)
 	}
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
 	})
 
-	http.ListenAndServe(":4000", mux)
+	http.ListenAndServe(":4000", r)
 }
