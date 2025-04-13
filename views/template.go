846c124a8cc6e771b799411776dab424685fe7f1
Alexandru Calin <alexandrucalin@proton.me>
2025-04-13 14:05:34 +0300

    Added currentUser custom template function

--- views/template.go
+++ views/template.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/alexandru-calin/galaria/context"
+	"github.com/alexandru-calin/galaria/models"
 	"github.com/gorilla/csrf"
 )
 
@@ -25,6 +27,9 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	tpl = tpl.Funcs(template.FuncMap{
 		"csrfField": func() (template.HTML, error) {
 			return "", fmt.Errorf("csrfField not implemented")
+		},
+		"currentUser": func() (*models.User, error) {
+			return nil, fmt.Errorf("currentUser not implemented")
 		},
 	})
 
@@ -64,6 +69,9 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data any) {
 	tpl = tpl.Funcs(template.FuncMap{
 		"csrfField": func() template.HTML {
 			return csrf.TemplateField(r)
+		},
+		"currentUser": func() *models.User {
+			return context.User(r.Context())
 		},
 	})
 
