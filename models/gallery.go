af5ee6a2f5aa51c4d9ea6fd434bc7c760aeb0cf3
Alexandru Calin <alexandrucalin@proton.me>
2025-04-15 16:52:18 +0300

    Created the gallery service

---
+++ models/gallery.go
@@ -0,0 +1,111 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
+type Gallery struct {
+	ID     int
+	UserID int
+	Title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (gs *GalleryService) Create(userID int, title string) (*Gallery, error) {
+	gallery := Gallery{
+		UserID: userID,
+		Title:  title,
+	}
+
+	row := gs.DB.QueryRow(`
+		INSERT INTO galleries (user_id, title)
+		VALUES ($1, $2)
+		RETURNING id`, gallery.UserID, gallery.Title)
+
+	err := row.Scan(&gallery.ID)
+	if err != nil {
+		return nil, fmt.Errorf("creating gallery: %w", err)
+	}
+
+	return &gallery, nil
+}
+
+func (gs *GalleryService) ByID(id int) (*Gallery, error) {
+	gallery := Gallery{
+		ID: id,
+	}
+
+	row := gs.DB.QueryRow(`
+		SELECT user_id, title FROM galleries WHERE id=$1`, gallery.ID)
+
+	err := row.Scan(&gallery.UserID, &gallery.Title)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+
+		return nil, fmt.Errorf("query gallery by id: %w", err)
+	}
+
+	return &gallery, nil
+}
+
+func (gs *GalleryService) ByUserID(userID int) ([]Gallery, error) {
+	rows, err := gs.DB.Query(`
+		SELECT id, title FROM galleries WHERE user_id=$1`, userID)
+
+	if err != nil {
+		return nil, fmt.Errorf("query galleries by user: %w", err)
+	}
+
+	var galleries []Gallery
+
+	for rows.Next() {
+		gallery := Gallery{
+			UserID: userID,
+		}
+
+		err := rows.Scan(&gallery.ID, &gallery.Title)
+		if err != nil {
+			return nil, fmt.Errorf("query galleries by user: %w", err)
+		}
+
+		galleries = append(galleries, gallery)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("query galleries by user: %w", err)
+	}
+
+	return galleries, nil
+}
+
+func (gs *GalleryService) Update(gallery *Gallery) error {
+	_, err := gs.DB.Exec(`
+		UPDATE galleries
+		SET title=$2
+		WHERE id=$1`, gallery.ID, gallery.Title)
+
+	if err != nil {
+		return fmt.Errorf("updating gallery: %w", err)
+	}
+
+	return nil
+}
+
+func (gs *GalleryService) Delete(id int) error {
+	_, err := gs.DB.Exec(`
+		DELETE FROM galleries WHERE id=$1`, id)
+
+	if err != nil {
+		return fmt.Errorf("deleting gallery: %w", err)
+	}
+
+	return nil
+}
