2966bdfbdd185dbb7fea1c1653e631b9fde09f14
Alexandru Calin <alexandrucalin@proton.me>
2025-04-17 13:48:18 +0300

    Created the gallery delete handler

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -50,26 +50,8 @@ func (g Galleries) Create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	gallery, err := g.galleryByID(w, r, userMustOwnGallery)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-
-	gallery, err := g.GalleryService.ByID(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNotFound) {
-			http.NotFound(w, r)
-			return
-		}
-
-		http.Error(w, "Oops, something went wrong...", http.StatusInternalServerError)
-		return
-	}
-
-	user := context.User(r.Context())
-	if user.ID != gallery.UserID {
-		http.Error(w, "You are not authorized to edit this gallery", http.StatusForbidden)
 		return
 	}
 
@@ -84,26 +66,8 @@ func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
 }
 
 func (g Galleries) Update(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	gallery, err := g.galleryByID(w, r, userMustOwnGallery)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-
-	gallery, err := g.GalleryService.ByID(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNotFound) {
-			http.NotFound(w, r)
-			return
-		}
-
-		http.Error(w, "Oops, something went wrong...", http.StatusInternalServerError)
-		return
-	}
-
-	user := context.User(r.Context())
-	if user.ID != gallery.UserID {
-		http.Error(w, "You are not authorized to edit this gallery", http.StatusForbidden)
 		return
 	}
 
@@ -120,20 +84,8 @@ func (g Galleries) Update(w http.ResponseWriter, r *http.Request) {
 }
 
 func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	gallery, err := g.galleryByID(w, r)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-
-	gallery, err := g.GalleryService.ByID(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNotFound) {
-			http.NotFound(w, r)
-			return
-		}
-
-		http.Error(w, "Oops, something went wrong...", http.StatusInternalServerError)
 		return
 	}
 
@@ -151,6 +103,21 @@ func (g Galleries) Show(w http.ResponseWriter, r *http.Request) {
 	}
 
 	g.Templates.Show.Execute(w, r, data)
+}
+
+func (g Galleries) Delete(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r, userMustOwnGallery)
+	if err != nil {
+		return
+	}
+
+	err = g.GalleryService.Delete(gallery.ID)
+	if err != nil {
+		http.Error(w, "Oops, something went wrong...", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/galleries", http.StatusFound)
 }
 
 func (g Galleries) Index(w http.ResponseWriter, r *http.Request) {
@@ -178,4 +145,44 @@ func (g Galleries) Index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	g.Templates.Index.Execute(w, r, data)
+}
+
+type galleryOpt func(w http.ResponseWriter, r *http.Request, gallery *models.Gallery) error
+
+func (g Galleries) galleryByID(w http.ResponseWriter, r *http.Request, opts ...galleryOpt) (*models.Gallery, error) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return nil, err
+	}
+
+	gallery, err := g.GalleryService.ByID(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			http.NotFound(w, r)
+			return nil, err
+		}
+
+		http.Error(w, "Oops, something went wrong...", http.StatusInternalServerError)
+		return nil, err
+	}
+
+	for _, opt := range opts {
+		err := opt(w, r, gallery)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return gallery, nil
+}
+
+func userMustOwnGallery(w http.ResponseWriter, r *http.Request, gallery *models.Gallery) error {
+	user := context.User(r.Context())
+	if user.ID != gallery.UserID {
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return fmt.Errorf("user does not own the gallery")
+	}
+
+	return nil
 }
